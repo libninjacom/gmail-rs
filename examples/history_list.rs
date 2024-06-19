72fed835ee38d22c5ba2d1e5548a690703c3639d
Kurt Wolf <kurtwolfbuilds@gmail.com>
2024-06-18 18:17:47 -0700

    fix

--- examples/history_list.rs
+++ examples/history_list.rs
@@ -6,12 +6,11 @@ async fn main() {
     let client = GmailClient::from_env();
     let user_id = "your user id";
     let response = client
-        .history_list(user_id)
+        .history_list(user_id, "history id")
         .history_types(&["your history types"])
         .label_id("your label id")
         .max_results(1)
         .page_token("your page token")
-        .start_history_id("your start history id")
         .await
         .unwrap();
     println!("{:#?}", response);
