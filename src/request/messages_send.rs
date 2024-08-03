01650bfb7245c56835d4279dd8a205eedcf9c221
Joey <joey.caffrey.24@gmail.com>
2024-08-03 14:34:15 -0700

    wip

--- src/request/messages_send.rs
+++ src/request/messages_send.rs
@@ -1,3 +1,7 @@
+use std::vec;
+
+use base64::{engine::general_purpose, Engine as _};
+
 use serde_json::json;
 use crate::model::*;
 use crate::FluentRequest;
@@ -10,6 +14,8 @@ On request success, this will return a [`Message`].*/
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct MessagesSendRequest {
     pub user_id: String,
+    pub message: String,
+    // TODO(joey): docs say The special value `me` can be used to indicate the authenticated user. do we wana default to that?
 }
 impl MessagesSendRequest {}
 impl FluentRequest<'_, MessagesSendRequest> {}
@@ -21,8 +27,15 @@ impl<'a> ::std::future::IntoFuture for FluentRequest<'a, MessagesSendRequest> {
             let url = &format!(
                 "/gmail/v1/users/{user_id}/messages/send", user_id = self.params.user_id
             );
+            dbg!(url);
             let mut r = self.client.client.post(url);
-            r = r.set_query(self.params);
+            // r = r.set_query(self.params);
+
+            r = r.body(httpclient::InMemoryBody::Text(json!({"raw": self.params.message}).to_string()));            
+            // r = r.body(httpclient::InMemoryBody::Text(json!({"message": {"raw": self.params.message}}).to_string()));            
+
+            // dbg!(&r);
+            
             r = self.client.authenticate(r);
             let res = r.await?;
             res.json().map_err(Into::into)
